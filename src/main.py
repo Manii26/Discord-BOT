0b7b9ec9a6868ed5e54eda51f5a5599c1a088fe7
Manon Cossou <manoncossou@MacBook-Air-de-Manon.local>
2022-09-21 22:00:46 +0200

    first push

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 import discord
+from random import *
+
 
 intents = discord.Intents.default()
 intents.members = True
@@ -10,16 +12,55 @@ bot = commands.Bot(
     intents = intents # Set up basic permissions
 )
 
-bot.author_id = 0000000  # Change to your discord id
+bot.author_id = 652839290325172224 # Change to your discord id
 
 @bot.event
 async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
+    
+
 @bot.command()
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_TOKEN>"
+@bot.command()
+async def name(ctx):
+    author = ctx.message.author
+    await ctx.send(author.name)
+
+@bot.command()
+async def d6(ctx):
+    n = randint(1,6)
+    await ctx.send(n)
+
+@bot.event
+async def on_message(message):
+    words = ['"Salut tout le monde"']
+
+    if message.content in words: 
+        await message.channel.send('"Salut tout seul"')
+
+
+  
+@bot.command(name='ban')
+async def ban(ctx, member : discord.Member, reason=None):
+    if reason == None:
+        await ctx.send(f"Oh oh {ctx.author.mention} don't provide a reason!")
+    else:
+        the_message = f"You have been banned from {ctx.guild.name} for {reason}"
+        await member.send(the_message)
+        await member.ban(reason=reason)
+
+@bot.tree.command()
+async def xkcd(ctx):
+    embed = discord.Embed()
+    embed.set_image(url="https://xkcd.com/2674/")
+
+    await interaction.response.send_message(embed=embed) 
+
+token = ""
 bot.run(token)  # Starts the bot
+
+
